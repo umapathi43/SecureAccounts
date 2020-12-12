ca538e8175d9ed6f831c1ce1309c6737b2f559ad
Sarvesh <sanepallisarvesh@gmail.com>
2020-12-12 12:15:54 +0530

    purchase screen

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -12,10 +12,64 @@ export class Purchase {
   styleUrls: ["./purchase.component.scss"],
 })
 export class PurchaseComponent implements OnInit {
+  showFields: any;
   constructor() {}
+  public Items: any[] = [
+    {
+      name: "",
+      batch: "",
+      expiryDate: "",
+      mfgDate: "",
+      bestBefore: "",
+      qty: "",
+      freeQty: "",
+      mrp: "",
+      purchaseDate: "",
+      discount: "",
+      discAmount: "",
+      schdiscAmount: "",
+      gst: "",
+      taxAmount: "",
+      qpk: "",
+      srt: "",
+      grossAmt: "",
+      netAmt: "",
+    },
+  ];
   model = new Purchase();
   ngOnInit(): void {}
   onSubmit(form) {
     console.log(form.value);
+  }
+  addItem() {
+    this.Items.push({
+      name: "",
+      batch: "",
+      expiryDate: "",
+      mfgDate: "",
+      bestBefore: "",
+      qty: "",
+      freeQty: "",
+      mrp: "",
+      purchaseDate: "",
+      discount: "",
+      discAmount: "",
+      schdiscAmount: "",
+      gst: "",
+      taxAmount: "",
+      qpk: "",
+      srt: "",
+      grossAmt: "",
+      netAmt: "",
+    });
+  }
+
+  removeItem(i: number) {
+    this.Items.splice(i, 1);
+  }
+
+  DisplayFileds(index) {
+    this.showFields = index;
+    console.log(this.showFields);
   }
 }
