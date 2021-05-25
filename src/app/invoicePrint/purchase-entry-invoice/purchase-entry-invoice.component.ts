0b90bef8cd8353ecb2e0fa49d9ea6976016a6923
kalyanreddy1991@gmail.com <kalyanreddy1991@gmail.com>
2021-05-25 16:02:11 +0530

    invoice print csss

--- src/app/invoicePrint/purchase-entry-invoice/purchase-entry-invoice.component.ts
+++ src/app/invoicePrint/purchase-entry-invoice/purchase-entry-invoice.component.ts
@@ -8,10 +8,62 @@ import { Component, OnInit } from "@angular/core";
 export class PurchaseEntryInvoiceComponent implements OnInit {
   active = 1;
   activeTab = "general";
+  titleText = "Sale Challan";
+  emailColor = "rgb(9, 72, 179)";
+  headercolor = "rgb(255, 255, 255)";
+  rowFontSize = "12px";
+  pringPadding = ".1in";
+  leftMargin = ".1";
+  rightMargin = ".1";
+  topMargin = ".1";
+  bottomMargin = ".1";
   constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
   activeswitch(action) {
     this.activeTab = action;
+  }
+  marginChange(event, action) {
+    if (action == "left") {
+      this.pringPadding =
+        this.topMargin +
+        "in " +
+        this.rightMargin +
+        "in " +
+        this.bottomMargin +
+        "in " +
+        event +
+        "in";
+    } else if (action == "right") {
+      this.pringPadding =
+        this.topMargin +
+        "in " +
+        event +
+        "in " +
+        this.bottomMargin +
+        "in " +
+        this.leftMargin +
+        "in";
+    } else if (action == "top") {
+      this.pringPadding =
+        event +
+        "in " +
+        this.rightMargin +
+        "in " +
+        this.bottomMargin +
+        "in " +
+        this.leftMargin +
+        "in";
+    } else {
+      this.pringPadding =
+        this.topMargin +
+        "in " +
+        this.rightMargin +
+        "in " +
+        event +
+        "in " +
+        this.leftMargin +
+        "in";
+    }
   }
 }
