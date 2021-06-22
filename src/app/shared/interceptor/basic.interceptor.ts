1fa15a640f5b311725a0a6b89c7d6b824b788377
kalyanreddy1991@gmail.com <kalyanreddy1991@gmail.com>
2021-06-22 23:32:34 +0530

    ip addresh changes in interceptor

--- src/app/shared/interceptor/basic.interceptor.ts
+++ src/app/shared/interceptor/basic.interceptor.ts
@@ -10,18 +10,17 @@ import {
 } from "@angular/common/http";
 import { Observable, of, throwError } from "rxjs";
 import { delay, mergeMap, materialize, dematerialize } from "rxjs/operators";
+import { environment } from "environments/environment";
 
 @Injectable()
 export class BasicAuthInterceptor implements HttpInterceptor {
   constructor(public auth: AuthService) {}
+  public ip = environment;
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if (
-      req.url == "http://54.157.75.0:9191/authenticate" &&
-      req.method === "POST"
-    ) {
+    if (req.url == this.ip.ApiURL + "/authenticate" && req.method === "POST") {
       return next.handle(req);
     } else {
       const userToken = this.auth.forLocalstorage();
